144376d0c7dc020879262fead991ab4c7eac6222
Britni <britni@users.noreply.github.com>
2016-12-12 12:05:17 -0500

    Update guess_game.rb

--- guess_game.rb
+++ guess_game.rb
@@ -1,7 +1,29 @@
+def hold_guess(guess, the_array)
+  the_array << guess
+end
+
+def extra_text_low(c1, c2)
+  if c1 > c2 then
+    puts "Umm, are you listening?"
+  end
+  if c1 == c2 then
+    puts "Same number silly!"
+  end
+end
+
+def extra_text_high(c1, c2)
+  if c1 < c2 then
+    puts "Umm, are you listening?"
+  end
+  if c1 == c2 then
+    puts "Same number silly!"
+  end
+end
+
 my_number = (rand * 100).to_i
 your_number = gets.chomp.to_i
 array = []
-array << your_number
+hold_guess(your_number, array)
 puts array
 latest_array = []
 
@@ -9,45 +31,32 @@ while your_number != my_number && array.length < 5 do
 
   if my_number > your_number then
 
-    puts "your number is too low. Try again"
+    puts "Your number is too low. Try again."
     if array.length < 5 then
       your_number = gets.chomp.to_i
-      array << your_number
+      hold_guess(your_number, array)
     end
-    puts array
-    puts array.last(2)
     latest_array = array.last(2)
-    puts latest_array
-    if latest_array [0] > latest_array [1] then
-      puts "Umm, are you listening?"
-    end
-    if latest_array [1] == latest_array [0] then
-      puts "Same number silly!"
-    end
+    extra_text_low(latest_array[0], latest_array[1])
   end
 
   if my_number < your_number then
-    puts "your number is too high"
+    puts "Your number is too high."
     if array.length < 5 then
       your_number = gets.chomp.to_i
-      array << your_number
+      hold_guess(your_number, array)
     end
-    puts array
+
     latest_array = array.last(2)
-    if latest_array [0] < latest_array [1] then
-      puts "Umm, are you listening?"
-    end
-    if latest_array [1] == latest_array [0] then
-      puts "Same number silly!"
-    end
+    extra_text_high(latest_array[0], latest_array[1])
   end
 
   if my_number == your_number then
-    puts "your number is equal to mine. You win!"
+    puts "Your number is equal to mine. You win!"
     puts array
   end
 end
 
 if my_number != your_number && array.length == 5 then
-  puts "you lose my number was #{my_number}"
+  puts "You lose. My number was #{my_number}"
 end
